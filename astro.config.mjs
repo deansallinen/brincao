88ea8d0ddde6f8fa4a362000b0e377065b24e33d
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-11-05 11:45:18 -0800

    Faster images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://brincao.com.br",
+  prefetch: {
+    prefetchAll: true,
+  },
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
+});
