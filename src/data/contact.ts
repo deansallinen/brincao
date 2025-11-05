56e4b18c0fe1aa1d04c7bcf7134f7fa6d12fd3c3
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-11-04 16:09:32 -0800

    migrate to astro

---
+++ src/data/contact.ts
@@ -0,0 +1,6 @@
+export default {
+  twitter: "@brincao",
+  facebook: "/brincao",
+  whatsapp: "+55 51 98112-2115",
+  instagram: "@brincaocreche",
+};
---
+++ src/data/pricing.ts
@@ -0,0 +1,12 @@
+export default {
+  year_updated: 2025,
+  hospedagem: {
+    diaria: 63,
+    dezoumais: 55,
+    finaldesemana: 98,
+  },
+  individuais: {
+    umdiaria: 45,
+    cincodias: 195,
+  },
+};
